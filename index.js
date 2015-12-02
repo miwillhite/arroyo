c4c9b7ac8a5afa5e542061a5f6046ee1bffcfe9f
Matt Willhite <matthew.willhite@gmail.com>
2015-12-02 18:00:23 -0500

    Basic transform and data combinator

---
+++ index.js
@@ -0,0 +1,66 @@
+'use strict';
+
+import {
+  ap,
+  combine,
+  merge,
+  on,
+  stream,
+  transduce
+} from 'flyd';
+
+import {
+  always,
+  call,
+  compose,
+  head,
+  identity,
+  is,
+  keys,
+  lift,
+  map,
+  nth,
+  of,
+  reverse,
+  sortBy,
+  toPairs,
+  unless,
+  unnest
+} from 'ramda';
+
+const log = console.log;
+
+const transformers = [
+  identity,
+  reverse,
+  lift(compose(unnest, toPairs))
+];
+
+const data = [
+  { a: 'z' },
+  { b: 'y' },
+  { c: 'x' },
+  { d: 'w' },
+  { e: 'v' },
+  { f: 'u' }
+];
+
+const inputStream = stream(data);
+const xformStream = stream();
+
+const dataStream = combine((xform, d) => {
+  return call(xform(), d());
+}, [xformStream, inputStream]);
+
+// Hook
+on((d) => log(`Results: ${JSON.stringify(d)}`), dataStream);
+
+// Testing
+let xform;
+while (xform = transformers.shift()) {
+  xformStream(xform);
+}
+
+// Results: [{"a":"z"},{"b":"y"},{"c":"x"},{"d":"w"},{"e":"v"},{"f":"u"}]
+// Results: [{"f":"u"},{"e":"v"},{"d":"w"},{"c":"x"},{"b":"y"},{"a":"z"}]
+// Results: [["a","z"],["b","y"],["c","x"],["d","w"],["e","v"],["f","u"]]
